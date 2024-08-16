7b0a91ac06f847dd5603bae0bfed9ec6d9ff8993
Nataliya <nataliyasjmonova@gmail.com>
2024-08-16 21:35:09 +0200

    make application Eureka client

--- src/main/java/com/springrest/springrest/entity/Student.java
+++ src/main/java/com/springrest/springrest/entity/Student.java
@@ -3,14 +3,16 @@ package com.springrest.springrest.entity;
 public class Student {
     private String firstName;
     private String lastName;
+    private String groupName;
 
     public Student() {
 
     }
 
-    public Student(String firstName, String lastName) {
+    public Student(String firstName, String lastName, String groupName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.groupName = groupName;
     }
 
     public String getFirstName() {
@@ -27,5 +29,13 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
     }
 }
